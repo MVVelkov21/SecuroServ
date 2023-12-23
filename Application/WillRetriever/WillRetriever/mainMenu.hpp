b3029dcb8c73d2f40dc355ac8e6a7e7daa2ad755
MVVelkov21 <MVVelkov21@codingburgas.bg>
2023-12-23 23:02:32 +0200

    Add simple database and login

--- Application/WillRetriever/WillRetriever/mainMenu.hpp
+++ Application/WillRetriever/WillRetriever/mainMenu.hpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <string>
+#include <sqlite3.h>
 
 using namespace std;
 
 class mainMenu {
 private:
+	sqlite3* db;
+	bool isUserExists(const string& username, sqlite3* db);
+	bool checkUser(sqlite3* db);
+	void saveLoginInfo(const string& username, const string& password, sqlite3* db);
 public:
 	 void menu();
 };
