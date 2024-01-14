31a63af0abcd45b3803126eda5b50d2787b35613
MMStoyanov21 <MMStoyanov21@codingburgas.bg>
2024-01-14 19:10:46 +0200

    Add feedback code

---
+++ Application/Feedback/Feedback.py
@@ -0,0 +1,17 @@
+import os
+
+print("  _____ _____ _____ ____  ____    _    ____ _  __")
+print(" |  ___| ____| ____|  _ \| __ )  / \  / ___| |/ /")
+print(" | |_  |  _| |  _| | | | |  _ \ / _ \| |   | ' / ")
+print(" |  _| | |___| |___| |_| | |_) / ___ \ |___| . \ ")
+print(" |_|   |_____|_____|____/|____/_/   \_\____|_|\_\\")
+print("                                                 ")
+choice = int(input("If you want to write your feedback enter 1, if not enter 2: "))
+if choice==1:
+    os.system("cls")
+    Name = str(input("Enter your name please: "))
+    Email = str(input("Enter your email: "))
+        
+else:
+    os.system("cls")
+
